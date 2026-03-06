744cc50b00a1e14b6b08b8e20db4002dbd58c16c
Maxim Moroz <maxm.morz@gmail.com>
2026-03-06 15:08:46 +0500

    chore: complete adyen to plexy rebranding
    Rename remaining variable references in server.js:
    - adyenWebServer → plexyWebServer
    - adyenWebExpress → plexyWebExpress

--- packages/server/.netlify/functions/server.js
+++ packages/server/.netlify/functions/server.js
@@ -1,10 +1,10 @@
 const serverless = require('serverless-http');
 const express = require('express');
-const adyenWebServer = require('../../index.js');
+const plexyWebServer = require('../../index.js');
 
 const app = express();
 
-const adyenWebExpress = adyenWebServer(app);
+const plexyWebExpress = plexyWebServer(app);
 
 // Export the handler function with the correct name
-module.exports.handler = serverless(adyenWebExpress);
+module.exports.handler = serverless(plexyWebExpress);
